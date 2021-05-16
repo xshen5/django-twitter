d86f5e26f6f196b680e05433980a48b73ae47f81
Xuanhong Shen <xuanhongshen@Xuanhongs-MBP.home>
2021-05-16 15:20:28 -0400

    6-tweet-model

--- tweets/models.py
+++ tweets/models.py
@@ -8,6 +8,7 @@ class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
     # updated_at = models.DateTimeField(auto_now=True)
 
     @property
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from tweets.models import Tweet
+from datetime import timedelta
+from utils.time_helpers import utc_now
 
-# Create your tests here.
+# Create a test for hours_to_now in Tweet model
+
+
+class TweetTests(TestCase):
+
+    def test_hours_to_now(self):
+        userone = User.objects.create_user(username='userone')
+        tweet = Tweet.objects.create(user=userone, content='test tweet')
+        tweet.created_at = utc_now() - timedelta(hours=10)
+        tweet.save()
+        self.assertEqual(tweet.hours_to_now, 36000)
