da47db140104c10c4b3931367fd326776a63a1de
Xuanhong Shen <xuanhongshen@Xuanhongs-MBP.home>
2021-05-16 22:17:22 -0400

    8-friendships-model-api-unit-test

---
+++ friendships/api/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers, exceptions
+
+from friendships.models import Friendship
+from accounts.api.serializers import UserSerializer
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    user = UserSerializer(source='from_user')
+    created_at = serializers.DateTimeField()
+
+    class Meta:
+        model = Friendship
+        fields = ('user', 'created_at')
+
+
+class FollowingSerializer(serializers.ModelSerializer):
+    user = UserSerializer(source='to_user')
+    created_at = serializers.DateTimeField()
+
+    class Meta:
+        model = Friendship
+        fields = ('user', 'created_at')
+
+
+class FriendshipSerializerForCreate(serializers.ModelSerializer):
+    from_user_id = serializers.IntegerField()
+    to_user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Friendship
+        fields = ('from_user_id', 'to_user_id')
+
+    def validate(self, attrs):
+        if attrs['from_user_id'] == attrs['to_user_id']:
+            raise exceptions.ValidationError({
+                'message': 'from_user_id and to_user_id must be different.'
+            })
+        return attrs
+
+    def create(self, validated_data):
+        from_user_id = validated_data['from_user_id']
+        to_user_id = validated_data['to_user_id']
+        return Friendship.objects.create(
+            from_user_id=from_user_id,
+            to_user_id=to_user_id,
+        )
