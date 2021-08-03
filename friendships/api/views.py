add68a033535c8e512288b40407a1f5fb101c459
Xuanhong Shen <xuanhongshen@xuanhongs-mbp.myfiosgateway.com>
2021-08-02 20:21:39 -0400

    support frienship pagination

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-
+from friendships.api.paginations import FriendshipPagination
 from friendships.models import Friendship
 from friendships.api.serializers import (
     FollowerSerializer,
@@ -19,24 +19,21 @@ class FriendshipViewSet(viewsets.GenericViewSet):
     # it because when file a request with details=True, it will call get_object()
     # which essentially calling objects.filter(pk=1) to check if the corresponding object exists
     queryset = User.objects.all()
+    pagination_class = FriendshipPagination
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followers(self, request, pk):
         friendships = Friendship.objects.filter(to_user_id=pk).order_by('-created_at')
-        serializer = FollowerSerializer(friendships, many=True)
-        return Response(
-            {'followers': serializer.data},
-            status=status.HTTP_200_OK,
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowerSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followings(self, request, pk):
         friendships = Friendship.objects.filter(from_user_id=pk).order_by('-created_at')
-        serializer = FollowingSerializer(friendships, many=True)
-        return Response(
-            {'followings': serializer.data},
-            status=status.HTTP_200_OK,
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowingSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
