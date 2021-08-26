edc2b89795290c45761dc6e0daa859b11c7ca560
Xuanhong Shen <xuanhongshen@xuanhongs-mbp.myfiosgateway.com>
2021-08-25 20:50:30 -0400

    add redis cache to likes count and comments count

--- comments/api/serializers.py
+++ comments/api/serializers.py
@@ -4,6 +4,7 @@ from likes.services import LikeService
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from tweets.models import Tweet
+from utils.redis_helper import RedisHelper
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -16,7 +17,7 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ('id', 'tweet_id', 'user', 'content', 'created_at', 'likes_count', 'has_liked',)
 
     def get_likes_count(self,obj):
-        return obj.like_set.count()
+        return RedisHelper.get_count(obj, 'likes_count')
 
     def get_has_liked(self, obj):
         return LikeService.has_liked(self.context['request'].user, obj)
--- likes/listeners.py
+++ likes/listeners.py
@@ -1,3 +1,5 @@
+from utils.redis_helper import RedisHelper
+
 
 def incr_likes_count(sender, instance, created, **kwargs):
     from tweets.models import Tweet
@@ -11,9 +13,11 @@ def incr_likes_count(sender, instance, created, **kwargs):
     if model_class == Comment:
         comment = instance.content_object
         Comment.objects.filter(id=comment.id).update(likes_count=F('likes_count') + 1)
+        RedisHelper.incr_count(comment, 'likes_count')
     if model_class == Tweet:
         tweet = instance.content_object
         Tweet.objects.filter(id=tweet.id).update(likes_count=F('likes_count') + 1)
+        RedisHelper.incr_count(tweet, 'likes_count')
 
 
 def decr_likes_count(sender, instance, **kwargs):
@@ -25,6 +29,8 @@ def decr_likes_count(sender, instance, **kwargs):
     if model_class == Comment:
         comment = instance.content_object
         Comment.objects.filter(id=comment.id).update(likes_count=F('likes_count') + 1)
+        RedisHelper.incr_count(comment, 'likes_count')
     if model_class == Tweet:
         tweet = instance.content_object
         Tweet.objects.filter(id=tweet.id).update(likes_count=F('likes_count') - 1)
+        RedisHelper.decr_count(tweet, 'likes_count')
