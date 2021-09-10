ed32310e49434969cdd9fd93b21b27f2c300741d
Xuanhong Shen <xuanhongshen@xuanhongs-mbp.myfiosgateway.com>
2021-09-09 21:50:11 -0400

    add testing for hbase

--- django_hbase/models/fields.py
+++ django_hbase/models/fields.py
@@ -1,11 +1,11 @@
 class HBaseField:
     field_type = None
 
-    def __int__(self, reverse=False, column_family=None, is_required=True, default=None):
+    def __init__(self, reverse=False, column_family=None):
         self.reverse = reverse
         self.column_family = column_family
-        self.is_required = is_required
-        self.default = default
+        #self.is_required = is_required
+        #self.default = default
 
         # TO-DO:
         # add is_required attribute, default=True, and attribtue default, default=None
@@ -18,8 +18,9 @@ class IntegerField(HBaseField):
     def __init__(self, *args, **kwargs):
         super(IntegerField, self).__init__(*args, **kwargs)
 
+
 class TimestampField(HBaseField):
     field_type = 'timestamp'
 
-    def __init__(self, *args, auto_now_add=False, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(TimestampField, self).__init__(*args, **kwargs)
